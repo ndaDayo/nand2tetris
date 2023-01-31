60e71c58f0e6996200636ec5c8179e66d6cedee9
ndaDayo <hayato711+nda@gmail.com>
2023-01-31 15:21:30 +0900

    [add] code comp api

--- projects/06/assembler/code/code.go
+++ projects/06/assembler/code/code.go
@@ -1,5 +1,7 @@
 package code
 
+import "errors"
+
 type Code struct{}
 
 func New() *Code {
@@ -24,5 +26,68 @@ func (c *Code) Dest(token string) byte {
 		return 0b111
 	default:
 		return 0b000
+	}
+}
+
+func (c *Code) Comp(token string) (byte, error) {
+	switch token {
+	case "0":
+		return 0b0101010, nil
+	case "1":
+		return 0b0111111, nil
+	case "-1":
+		return 0b0111010, nil
+	case "D":
+		return 0b0001100, nil
+	case "A":
+		return 0b0110000, nil
+	case "!D":
+		return 0b0001101, nil
+	case "!A":
+		return 0b0110001, nil
+	case "-D":
+		return 0b0001111, nil
+	case "-A":
+		return 0b0110011, nil
+	case "D+1":
+		return 0b0011111, nil
+	case "A+1":
+		return 0b0110111, nil
+	case "D-1":
+		return 0b0001110, nil
+	case "A-1":
+		return 0b0110010, nil
+	case "D+A":
+		return 0b0000010, nil
+	case "D-A":
+		return 0b0010011, nil
+	case "A-D":
+		return 0b0000111, nil
+	case "D&A":
+		return 0b0000000, nil
+	case "D|A":
+		return 0b0010101, nil
+	case "M":
+		return 0b1110000, nil
+	case "!M":
+		return 0b1110001, nil
+	case "-M":
+		return 0b1110011, nil
+	case "M+1":
+		return 0b1110111, nil
+	case "M-1":
+		return 0b1110010, nil
+	case "D+M":
+		return 0b1000010, nil
+	case "D-M":
+		return 0b1010011, nil
+	case "M-D":
+		return 0b1000111, nil
+	case "D&M":
+		return 0b1000000, nil
+	case "D|M":
+		return 0b1010101, nil
+	default:
+		return 0b0, errors.New("invalid comp detected")
 	}
 }
--- projects/06/assembler/code/code_test.go
+++ projects/06/assembler/code/code_test.go
@@ -27,3 +27,53 @@ func TestDest(t *testing.T) {
 		}
 	}
 }
+
+type compTest struct {
+	in  string
+	out byte
+}
+
+func TestComp(t *testing.T) {
+	tests := []compTest{
+		{"0", 0b0101010},
+		{"1", 0b0111111},
+		{"-1", 0b0111010},
+		{"D", 0b0001100},
+		{"A", 0b0110000},
+		{"!D", 0b0001101},
+		{"!A", 0b0110001},
+		{"-D", 0b0001111},
+		{"-A", 0b0110011},
+		{"D+1", 0b0011111},
+		{"A+1", 0b0110111},
+		{"D-1", 0b0001110},
+		{"A-1", 0b0110010},
+		{"D+A", 0b0000010},
+		{"D-A", 0b0010011},
+		{"A-D", 0b0000111},
+		{"D&A", 0b0000000},
+		{"D|A", 0b0010101},
+		{"M", 0b1110000},
+		{"!M", 0b1110001},
+		{"-M", 0b1110011},
+		{"M+1", 0b1110111},
+		{"M-1", 0b1110010},
+		{"D+M", 0b1000010},
+		{"D-M", 0b1010011},
+		{"M-D", 0b1000111},
+		{"D&M", 0b1000000},
+		{"D|M", 0b1010101},
+	}
+
+	for i, test := range tests {
+		c := New()
+		comp, err := c.Comp(test.in)
+		if err != nil {
+			t.Errorf("#%d: error returned: %v", i, err.Error())
+		}
+
+		if comp != test.out {
+			t.Errorf("#%d: got: %v want: %v", i, comp, test.out)
+		}
+	}
+}
