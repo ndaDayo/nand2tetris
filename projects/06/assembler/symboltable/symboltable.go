b8420a4ad06ba51aa78ca0b08ee9069550391b0a
ndaDayo <hayato711+nda@gmail.com>
2023-02-01 12:06:31 +0900

    [add] addEntry

--- projects/06/assembler/symboltable/symboltable.go
+++ projects/06/assembler/symboltable/symboltable.go
@@ -1,1 +1,15 @@
 package symboltable
+
+type SymbolTable struct {
+	table map[string]int
+}
+
+func New() *SymbolTable {
+	return &SymbolTable{
+		make(map[string]int),
+	}
+}
+
+func (s *SymbolTable) AddEntry(symbol string, address int) {
+	s.table[symbol] = address
+}
