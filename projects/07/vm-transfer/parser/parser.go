3989e40947c423392b053442672e51ee589503f6
ndaDayo <hayato711+nda@gmail.com>
2023-02-12 15:40:18 +0900

    add CommandType

--- projects/07/vm-transfer/parser/parser.go
+++ projects/07/vm-transfer/parser/parser.go
@@ -1,5 +1,7 @@
 package parser
 
+import "strings"
+
 type Parser struct {
 	currentCommand string
 	lines          []string
@@ -12,4 +14,43 @@ func (p *Parser) HasMoreCommands() bool {
 func (p *Parser) Advance() {
 	p.currentCommand = p.lines[0]
 	p.lines = p.lines[1:]
+}
+
+type CommandTypes int
+
+const (
+	ArithmeticCommand CommandTypes = iota
+	PushCommand
+	PopCommand
+	LabelCommand
+	GotoCommand
+	IfCommand
+	FunctionCommand
+	ReturnCommand
+	CallCommand
+	UnknownCommand
+)
+
+func (p *Parser) CommandType() CommandTypes {
+	commandTypeMap := map[string]CommandTypes{
+		"push":     PushCommand,
+		"pop":      PopCommand,
+		"label":    LabelCommand,
+		"goto":     GotoCommand,
+		"if-goto":  IfCommand,
+		"function": FunctionCommand,
+		"call":     CallCommand,
+		"return":   ReturnCommand,
+	}
+
+	command := p.Command()
+	if cmdType, ok := commandTypeMap[command]; ok {
+		return cmdType
+	}
+
+	return ArithmeticCommand
+}
+
+func (p *Parser) Command() string {
+	return strings.Split(p.currentCommand, " ")[0]
 }
